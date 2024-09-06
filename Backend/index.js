d19ccde18c9f1c668995814ce8d26a8a74718258
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-09-06 11:15:54 -0500

    FIX: change the .env file position

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 
 //IMPORTS
-//Routes
-const routesHandler = require('./Routes/RoutesHandler');
+
+require('dotenv').config(); //DOTENV 
+
+const routesHandler = require('./Routes/RoutesHandler'); //ROUTES
 
 const app = express();
+
+//SETTING PORT
+const PORT = process.env.PORT || 3000;
+
  
-const PORT = 3000;
-
-
-
+ 
 
 //ROUTES HANDLER
 app.use( '/', routesHandler );
-
  
+  
 app.listen( PORT, () => {
-    console.log("server run on port 3000");
+    console.log(`Server running on port ${PORT}`);
 } )
  
